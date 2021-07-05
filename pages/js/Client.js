266fd6e857cc8911e9a881b9e8f5e89641132299
qurishif <faislqurishi@csus.edu>
2021-07-04 19:36:58 -0700

    solved server client chat issue

--- pages/js/Client.js
+++ pages/js/Client.js
@@ -5,6 +5,25 @@ const socket = io("http://52.8.58.114:3000", {
   transports: ['websocket'],
 });
 
+let messages = document.getElementById('messages');
+let form = document.getElementById('form');
+let input = document.getElementById('input');
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+  if (input.value) {
+    socket.emit('chat message', input.value);
+    input.value = '';
+  }
+});
+
+socket.on('chat message', function(msg) {
+  let item = document.createElement('li');
+  item.textContent = msg;
+  messages.appendChild(item);
+  window.scrollTo(0, document.body.scrollHeight);
+});
+
 socket.on("connect", () => {
   console.log("connecting");
   socket.emit('connection');
