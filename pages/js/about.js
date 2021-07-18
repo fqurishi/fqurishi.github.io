47e2d426db550632d99d466378ee1032f7d6e970
Faisl Q <faislqurishi@gmail.com>
2021-07-18 00:36:52 -0700

    fixed typos, updated about page, addded games page, added better graphics

--- pages/js/about.js
+++ pages/js/about.js
@@ -1,28 +1,51 @@
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.118/build/three.module.js';
 
 
+const container = document.getElementById('scene')
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera( 45, container.offsetWidth / container.offsetHeight, 0.5, 50 );
 
 const renderer = new THREE.WebGLRenderer( { antialias: true } );
 renderer.setPixelRatio( window.devicePixelRatio );
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+renderer.setSize( container.offsetWidth, container.offsetHeight );
+container.appendChild( renderer.domElement );
 
 //lights
 const light = new THREE.PointLight();
 light.position.set( 200, 100, 150 );
 scene.add( light );
 
-//cube
+//cube pic
 const texture = new THREE.TextureLoader().load( '../images/faislqurishi.jpg' ); 
-const geometry = new THREE.BoxBufferGeometry(2 , 2, 2); 
+const geometry = new THREE.BoxBufferGeometry(2.5 , 2.5, 2.5); 
 const material = new THREE.MeshBasicMaterial( {map: texture} );  
 const mesh = new THREE.Mesh( geometry, material ); 
 
-mesh.position.y += 0.5;
+mesh.position.y += 0.175;
+mesh.position.z -= 0.5;
 scene.add( mesh );
 
+//colors
+const blue = 0x4a03fc;
+const red = 0xfc0303;
+const yellow = 0xcd703;
+const white = 0x4d4d4;
+const orange = 0xff9500;
+const green = 0x0ed10a;
+const cyan = 0x05e3be;
+const colors = [blue, red, yellow, white, orange, green, cyan];
+
+//random objects to float behind pic
+const cubeArray = [];
+const sphereArray = []; 
+const coneArray = [];
+for (let i = 0; i < 20; i++){
+    addCubes();
+    addSpheres();
+    addTriangles();
+}
+
+//camera
 camera.position.z = 5;
 
 //window 
@@ -31,18 +54,71 @@ window.addEventListener( 'resize', onWindowResize, false );
 //animate
 animate();
 
+
+
 function onWindowResize() 
 { 
-camera.aspect = window.innerWidth / window.innerHeight; 
+camera.aspect = container.offsetWidth / container.offsetHeight; 
 camera.updateProjectionMatrix(); 
 
-renderer.setSize( window.innerWidth, window.innerHeight ); 
+renderer.setSize( container.offsetWidth, container.offsetHeight ); 
 } 
 
 function animate() 
 { 
 requestAnimationFrame( animate ); 
-mesh.rotation.y += 0.007; 
+mesh.rotation.y += 0.007;
+for (let i = 0; i < 20; i++){
+    cubeArray[i].rotation.x += 0.01;
+    cubeArray[i].position.x -= 0.0075;
+    cubeArray[i].rotation.z += 0.005;
+    sphereArray[i].rotation.x += 0.01;
+    sphereArray[i].position.x -= 0.0075;
+    sphereArray[i].rotation.z += 0.005;
+    coneArray[i].rotation.x += 0.01;
+    coneArray[i].position.x -= 0.0075;
+    coneArray[i].rotation.z += 0.005;
+}
 
 renderer.render( scene, camera ); 
+}
+
+
+function addCubes(){
+    const geometry = new THREE.BoxGeometry();
+    const material = new THREE.MeshBasicMaterial( { color: colors[THREE.MathUtils.randInt(0,6)], wireframe: true } );
+    const cube = new THREE.Mesh( geometry, material );
+    let x,y,z;
+    x = THREE.MathUtils.randFloat(-180,180);
+    y = THREE.MathUtils.randFloat(-1,3);
+    z = THREE.MathUtils.randFloat(-46,-8);
+    cube.position.set(x,y,z);
+    scene.add( cube );
+    cubeArray.push(cube);
+}
+function addSpheres(){
+    const geometry = new THREE.SphereGeometry();
+    const material = new THREE.MeshBasicMaterial( { color: colors[THREE.MathUtils.randInt(0,6)], wireframe: true } );
+    const sphere = new THREE.Mesh( geometry, material );
+    let x,y,z;
+    x = THREE.MathUtils.randFloat(-180,180);
+    y = THREE.MathUtils.randFloat(-1,3);
+    z = THREE.MathUtils.randFloat(-46,-8);
+    sphere.position.set(x,y,z);
+    scene.add( sphere );
+    sphereArray.push(sphere);
+
+}
+function addTriangles(){
+    const geometry = new THREE.ConeGeometry();
+    const material = new THREE.MeshBasicMaterial( { color: colors[THREE.MathUtils.randInt(0,6)], wireframe: true } );
+    const triangle = new THREE.Mesh( geometry, material );
+    let x,y,z;
+    x = THREE.MathUtils.randFloat(-180,180);
+    y = THREE.MathUtils.randFloat(-1,3);
+    z = THREE.MathUtils.randFloat(-46,-8);
+    triangle.position.set(x,y,z);
+    scene.add( triangle );
+    coneArray.push(triangle);
+
 }
--- pages/js/indexPage.js
+++ pages/js/indexPage.js
@@ -9,6 +9,16 @@ renderer.setPixelRatio( window.devicePixelRatio );
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
+//colors
+const blue = 0x4a03fc;
+const red = 0xfc0303;
+const yellow = 0xcd703;
+const white = 0x4d4d4;
+const orange = 0xff9500;
+const green = 0x0ed10a;
+const cyan = 0x05e3be;
+const colors = [blue, red, yellow, white, orange, green, cyan];
+
 //lights
 const light = new THREE.PointLight();
 light.position.set( 200, 100, 150 );
@@ -27,7 +37,7 @@ for (let i = 0; i < 500; i++){
 
 
 //grid
-const gridHelper = new THREE.GridHelper( 400, 40, 0x808080, 0x808080 );
+const gridHelper = new THREE.GridHelper( 400, 40, 0xbe03fc, 0xbe03fc );
 gridHelper.position.y = 0;
 gridHelper.position.x = 0;
 gridHelper.rotateX(0.1);
@@ -59,14 +69,18 @@ const animate = function () {
     renderer.render( scene, camera );
 };
 
+//window
+window.addEventListener( 'resize', onWindowResize, false ); 
+
+//animate
 animate();
 
 function addCubes(){
     const geometry = new THREE.BoxGeometry();
-    const material = new THREE.MeshBasicMaterial( { color: 0x808080, wireframe: true } );
+    const material = new THREE.MeshBasicMaterial( { color: colors[THREE.MathUtils.randInt(0,6)], wireframe: true } );
     const cube = new THREE.Mesh( geometry, material );
     let x,y,z;
-    x = THREE.MathUtils.randFloat(-200,200);
+    x = THREE.MathUtils.randFloat(-500,50);
     y = THREE.MathUtils.randFloat(9,38);
     z = THREE.MathUtils.randFloat(-26,30);
     cube.position.set(x,y,z);
@@ -75,10 +89,10 @@ function addCubes(){
 }
 function addSpheres(){
     const geometry = new THREE.SphereGeometry();
-    const material = new THREE.MeshBasicMaterial( { color: 0x808080, wireframe: true } );
+    const material = new THREE.MeshBasicMaterial( { color: colors[THREE.MathUtils.randInt(0,6)], wireframe: true } );
     const sphere = new THREE.Mesh( geometry, material );
     let x,y,z;
-    x = THREE.MathUtils.randFloat(-200,200);
+    x = THREE.MathUtils.randFloat(-500,50);
     y = THREE.MathUtils.randFloat(9,38);
     z = THREE.MathUtils.randFloat(-26,30);
     sphere.position.set(x,y,z);
@@ -88,10 +102,10 @@ function addSpheres(){
 }
 function addTriangles(){
     const geometry = new THREE.ConeGeometry();
-    const material = new THREE.MeshBasicMaterial( { color: 0x808080, wireframe: true } );
+    const material = new THREE.MeshBasicMaterial( { color: colors[THREE.MathUtils.randInt(0,6)], wireframe: true } );
     const triangle = new THREE.Mesh( geometry, material );
     let x,y,z;
-    x = THREE.MathUtils.randFloat(-200,200);
+    x = THREE.MathUtils.randFloat(-500,50);
     y = THREE.MathUtils.randFloat(9,38);
     z = THREE.MathUtils.randFloat(-26,30);
     triangle.position.set(x,y,z);
@@ -108,20 +122,4 @@ function onWindowResize() {
     renderer.setSize( window.innerWidth, window.innerHeight );
 
 }
-
-
-function openPage(pageName,elmnt,color) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("w3-bar-item w3-button w3-padding-large w3-hide-small");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].style.backgroundColor = "";
-    }
-    document.getElementById(pageName).style.display = "block";
-  }
   
-  // Get the element with id="defaultOpen" and click on it
-  document.getElementById("defaultOpen").click();
